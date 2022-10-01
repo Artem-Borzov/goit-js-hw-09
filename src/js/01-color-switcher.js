56dd4ca8b9e09924637a9c0832bb2fbd635ae064
Artem-Borzov <abug990@gmail.com>
2022-10-01 20:09:16 +0300

    task-1-completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startDisco = document.querySelector('button[data-start]');
+const stopDisco = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+// console.log(bodyEl);
+stopDisco.disabled = true;
+
+startDisco.addEventListener('click', discoAction);
+stopDisco.addEventListener('click', discoStop);
+
+function discoAction() {
+  timerID = setInterval(backgroundColorChange, 1000);
+  startDisco.disabled = true;
+  stopDisco.disabled = false;
+  return timerID;
+}
+
+function discoStop() {
+  startDisco.disabled = false;
+  stopDisco.disabled = true;
+  clearInterval(timerID);
+}
+
+function backgroundColorChange() {
+  const backgroundColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = backgroundColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
