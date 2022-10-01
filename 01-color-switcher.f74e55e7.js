437401345321c0af9178b0a2b0167bed00a000d2
Artem-Borzov <95220251+Artem-Borzov@users.noreply.github.com>
2022-10-01 17:09:37 +0000

    Deploying to gh-pages from @ Artem-Borzov/goit-js-hw-09@56dd4ca8b9e09924637a9c0832bb2fbd635ae064 🚀

---
+++ 01-color-switcher.f74e55e7.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");function r(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.backgroundColor=t}e.disabled=!0,t.addEventListener("click",(function(){return timerID=setInterval(r,1e3),t.disabled=!0,e.disabled=!1,timerID})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(timerID)}))}();
+//# sourceMappingURL=01-color-switcher.f74e55e7.js.map
